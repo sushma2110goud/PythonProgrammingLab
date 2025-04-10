04d1857dfd2466432d925b2478e552a285079e99
sushma2110goud <sushmagoud03@gmail.com>
2025-04-10 10:50:49 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,18 @@
+l1=[]
+rangeofl1=int(input("enter the elements"))
+for i in range(rangeofl1):
+    temp=int(input())
+    l1.append(temp)
+print(l1)   
+l2=[]
+rangeofl2=int(input("enter the elements"))
+for i in range(rangeofl2):
+    temp=int(input())
+    l2.append(temp)
+print(l2)
+l3=[]
+rangeofl3=int(input("enter the elements"))
+for i in range(rangeofl3):
+    temp=int(input())
+    l3.append(temp)
+print(l3)
