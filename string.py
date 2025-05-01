f890182dd7afc6b960e866936f6fc02097e9229e
sushma2110goud <sushmagoud03@gmail.com>
2025-05-01 10:32:01 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,29 @@
+my_string = "Hello, world!"
+print(my_string[0:5:2])
+print(my_string[0:5])
+print(my_string[7:])
+print(my_string[5:])
+
+string1= "Hello"
+string2= "world"
+concatenated_string = string1+' '+string2
+print(concatenated_string)
+
+a = "Hello world"
+length = len(a)
+print(length)
+
+text = "Hello world"
+print(text.lower())
+print(text.upper())
+
+text = "Hello World"
+print(text.strip())
+print(text.split())
+
+a = "sushma, sumedha, akshara"
+print(a.split(","))
+
+text = "Hello world"
+print(text.replace("world" , "python"))
+
